81d01ee063327e6de89c351e3d82e5cc7de734fd
Creel Patrocinio <creel@hawaii.edu>
2022-04-17 20:36:43 -1000

    Classes added

--- Weight.h
+++ Weight.h
@@ -21,9 +21,47 @@ public:
     static const std::string KILO_LABEL;
     static const std::string SLUG_LABEL;
 
+    Weight() noexcept;
+    Weight(float newWeight);
+    Weight(UnitOfWeight newUnitOfWeight) noexcept;
+    Weight(float newWeight, UnitOfWeight newUnitOfWeight);
+    Weight(float newWeight, float newMaxWeight);
+    Weight(UnitOfWeight newUnitOfWeight, float newMaxWeight);
+    Weight(float newWeight, UnitOfWeight newUnitOfWeight, float newMaxWeight);
+
+    bool isWeightKnown() const noexcept;
+    bool hasMaxWeight() const noexcept;
+
+    float getWeight() const noexcept;
+    float getWeight(UnitOfWeight weightUnits) const noexcept;
+    float getMaxWeight() const noexcept;
+
+    UnitOfWeight getWeightUnit() const noexcept;
+    void setWeight(float newWeight);
+    void setWeight(float newWeight, UnitOfWeight weightUnits);
+
+    bool isWeightValid(float checkWeight) const noexcept;
+    bool validate() const noexcept;
+    void dump() const noexcept;
+    bool operator==(const Weight &rhs_Weight);
+    bool operator<(const Weight &rhs_Weight) const;
+
+    Weight & operator+=(float rhs_addToWeight);
+
+    static float fromKilogramToPound(float kilogram) noexcept;
+    static float fromPoundToKilogram(float pound) noexcept;
+    static float fromSlugToPound(float slug) noexcept;
+    static float fromPoundToSlug(float pound) noexcept;
+    static float convertWeight(float fromWeight, UnitOfWeight fromUnit, UnitOfWeight toUnit) noexcept;
+    void setMaxWeight(float newMaxWeight);
 
 
-
+private:
+    bool bIsKnown;
+    bool bHasMax;
+    enum UnitOfWeight unitOfWeight;
+    float weight;
+    float maxWeight;
 
 };
 
