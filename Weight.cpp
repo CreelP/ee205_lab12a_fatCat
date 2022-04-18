8a47629f5a59498e8013ec0f6ee80e842bb7f736
Creel Patrocinio <creel@hawaii.edu>
2022-04-17 20:30:19 -1000

    File creation

---
+++ Weight.cpp
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab12a_fatCat - EE 205 - Spr 2022
+///
+/// @file Weight.cpp
+/// @version 1.0
+///
+/// @author Creel Patrocinio <creel@hawaii.edu>
+/// @date   17_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+const float Weight::KILOS_IN_A_POUND = 0.453592 ;
---
+++ Weight.h
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab12a_fatCat - EE 205 - Spr 2022
+///
+/// @file Weight.h
+/// @version 1.0
+///
+/// @author Creel Patrocinio <creel@hawaii.edu>
+/// @date   17_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+#ifndef EE205_LAB12A_FATCAT_WEIGHT_H
+#define EE205_LAB12A_FATCAT_WEIGHT_H
+
+class Weight {
+
+public:
+    enum UnitOfWeight {POUND, KILO, SLUG};
+    static const float KILOS_IN_A_POUND;
+    static const float SLUGS_IN_A_POUND;
+    static const std::string POUND_LABEL;
+    static const std::string KILO_LABEL;
+    static const std::string SLUG_LABEL;
+
+
+
+
+
+};
+
+
+
+#endif //EE205_LAB12A_FATCAT_WEIGHT_H
